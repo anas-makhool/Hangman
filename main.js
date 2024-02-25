69e86edacde2456712e7fa75175382c5af0a6420
anas-makhool <anasmkhool@gmail.com>
2024-02-25 22:40:26 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Letters
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+// get array form letters
+let lettersArray = letters.split("");
+
+//select letters container
+let lettersContainer = document.querySelector(".letters");
+
