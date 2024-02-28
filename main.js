c89ce7063ca24e07fe19726d7da1fcacff979365
anas-makhool <anasmkhool@gmail.com>
2024-02-28 12:53:21 +0200

    the finalize

--- main.js
+++ main.js
@@ -27,82 +27,114 @@ lettersArray.forEach((letter) => {
 
 // object of words + categories
 
-const words = {
-  programming: [
-    "JavaScript",
-    "Python",
-    "Java",
-    "SQL",
-    "Ruby",
-    "Swift",
-    "Go",
-    "Rust",
-  ],
-  movies: [
-    "prestige",
-    "inception",
-    "parasite",
-    "interstellar",
-    "whiplash",
-    "memento",
-    "coco",
-    "up",
-  ],
-  people: [
-    "Nelson Mandela",
-    "Mother Teresa",
-    "Mahatma Gandhi",
-    "Martin Luther King Jr",
-    "Albert Einstein",
-  ],
-  countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
-};
+fetch("json.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // get random property
+    let allKeys = Object.keys(data);
 
-// get random property
-let allKeys = Object.keys(words);
+    let randomPropNumber = Math.floor(Math.random() * allKeys.length);
+    let randomPropName = allKeys[randomPropNumber];
+    let randomPropValue = data[randomPropName];
 
-let randomPropNumber = Math.floor(Math.random() * allKeys.length);
-let randomPropName = allKeys[randomPropNumber];
-let randomPropValue = words[randomPropName];
+    let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
+    let randomValueValue = randomPropValue[randomValueNumber];
 
-let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
-let randomValueValue = randomPropValue[randomValueNumber];
+    // set category info
+    document.querySelector(".game-info .category span").innerHTML =
+      randomPropName;
 
-// set category info
-document.querySelector(".game-info .category span").innerHTML = randomPropName;
+    // select letters guess element
+    let lettersGuessContainer = document.querySelector(".letters-guess");
 
-// select letters guess element
-let lettersGuessContainer = document.querySelector(".letters-guess");
+    // convert chosen world to array
+    let letterAndSpace = Array.from(randomValueValue);
 
-// convert chosen world to array
-let letterAndSpace = Array.from(randomValueValue);
+    console.log(randomPropName);
+    console.log(randomValueValue.toLowerCase());
 
-console.log(randomValueValue.toLowerCase());
-console.log(randomPropName);
-let theChosenWord = Array.from(randomValueValue.toLowerCase());
-
-// create spans depend on word
-theChosenWord.forEach((ele, ind) => {
-  let emptySpan = document.createElement("span");
-
-  ele === " " ? (emptySpan.className = "has-space") : (emptySpan.id = ind);
-
-  // append spans to letters guess container
-  lettersGuessContainer.appendChild(emptySpan);
-});
-
-// Handle clicking letters
-document.addEventListener("click", (e) => {
-  if (e.target.className === "letter-box") {
-    e.target.classList.add("clicked");
-
-    let theClickedLetter = e.target.innerHTML.toLowerCase();
-
+    // create spans depend on word
     letterAndSpace.forEach((ele, ind) => {
-      if (theClickedLetter === ele) {
-        console.log(`found at index number ${ind}`);
-        [...lettersGuessContainer.children][ind].innerHTML = ele
+      let emptySpan = document.createElement("span");
+
+      ele === " " ? (emptySpan.className = "has-space") : (emptySpan.id = ind);
+
+      // append spans to letters guess container
+      lettersGuessContainer.appendChild(emptySpan);
+    });
+
+    let guessSpans = document.querySelectorAll(".letters-guess span");
+    let wrongAttempts = 0;
+    let theDraw = document.querySelector(".hangman-draw");
+
+    // Handle clicking letters
+    document.addEventListener("click", (e) => {
+      let theStatus = false;
+      if (e.target.className === "letter-box") {
+        e.target.classList.add("clicked");
+
+        let theClickedLetter = e.target.innerHTML.toLowerCase();
+        let theChosenWord = Array.from(randomValueValue.toLowerCase());
+
+        theChosenWord.forEach((ele, ind) => {
+          if (theClickedLetter === ele) {
+            theStatus = true;
+            guessSpans.forEach((span, spanIndex) => {
+              if (ind === spanIndex) {
+                span.innerHTML = ele;
+              }
+            });
+          }
+        });
+
+        if (!theStatus) {
+          new Audio("/sounds/lost round.wav").play();
+          wrongAttempts++;
+          theDraw.classList.add(`wrong-${wrongAttempts}`);
+          if (theDraw.classList.contains("wrong-8")) {
+            lettersContainer.style.pointerEvents = "none";
+            new Audio("/sounds/loss all game.wav").play();
+            showPopup(`Oops! You Loss :/ , The Word Is ${randomValueValue}`);
+          } else {
+            lettersContainer.style.pointerEvents = "auto";
+          }
+        } else {
+          new Audio("/sounds/win round.wav").play();
+        }
+
+        if (
+          [...lettersGuessContainer.children]
+            .map((ele) => ele.innerHTML)
+            .join("")
+            .replaceAll(" ", "") ===
+          randomValueValue.toLowerCase().replaceAll(" ", "")
+        ) {
+          new Audio("/sounds/victory.wav").play();
+          if (wrongAttempts >= 5 && wrongAttempts < 8) {
+            showPopup(
+              `Good Game, The Word Is "${randomValueValue}", after ${wrongAttempts} Attempts`
+            );
+          } else if (wrongAttempts > 0 && wrongAttempts < 5) {
+            showPopup(
+              `Perfect, The Word Is "${randomValueValue}", after ${wrongAttempts} Attempts`
+            );
+          } else if (wrongAttempts === 0) {
+            showPopup(`Congratulations, The Word Is "${randomValueValue}"`);
+          }
+        }
+      }
+
+      function showPopup(message) {
+        let popup = document.getElementById("popup");
+        let popupMessage = popup.querySelector(".popup-message");
+
+        popupMessage.textContent = message;
+        popup.style.display = "block";
+        setTimeout(() => {
+          popup.style.display = "none";
+        }, 3000);
       }
     });
-  }
-});
+  })
+  .catch((error) => console.error("Error fetching data:", error));
